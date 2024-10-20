c246cfaf8a3dcad46fa4daefaabc6ae61148c091
Antonia Córdova <a.cordova02@ufromail.cl>
2024-10-20 13:52:48 -0300

    Edición de algunos métodos para responder a las preguntas de mejor manera, actualización de la ubicación del archivo readme

--- src/main/java/TestFecha.java
+++ src/main/java/TestFecha.java
@@ -2,34 +2,36 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 class TodaysDate {
+    // Variable que guarda el tiempo
+    private String time;
 
-    public void printDateAndTime() {
-        // objeto que obtiene la fecha y hora actuales
+    public TodaysDate() {
+        setCurrentTime();
+    }
+
+    private void setCurrentTime() {
         Calendar now = new GregorianCalendar();
-
-        // se consiguen las componentes de la fecha
         int year = now.get(Calendar.YEAR);
-        int month = now.get(Calendar.MONTH) + 1; //están indexado desde el 0, por lo que se debe sumar 1
+        int month = now.get(Calendar.MONTH) + 1;
         int day = now.get(Calendar.DAY_OF_MONTH);
         int hour = now.get(Calendar.HOUR_OF_DAY);
         int minute = now.get(Calendar.MINUTE);
         int second = now.get(Calendar.SECOND);
 
-        // agregamos el 02d para asegurarnos que el formato siempre sea de dos digitos
-        System.out.println("La fecha y hora actuales son: " + year + "-"
-                + String.format("%02d", month) + "-" + String.format("%02d", day) + " "
-                + String.format("%02d", hour) + ":" + String.format("%02d", minute) + ":"
-                + String.format("%02d", second));
+        // se agrega el formato en el que se quiere entregar cada dato
+        this.time = String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
+    }
+
+    public void printDateAndTime() {
+        System.out.println("La fecha y hora actuales son: " + this.time);
     }
 }
 
 public class TestFecha {
-
     public static void main(String[] args) {
-        // objeto de la clase TodaysDate
+
         TodaysDate hoy = new TodaysDate();
 
         hoy.printDateAndTime();
     }
 }
-
