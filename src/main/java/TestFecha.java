1094456193fbea770eb46b95b5086c60d9bee5ba
AlexCerileRojas <a.rojas13@ufromail.cl>
2024-10-21 21:59:45 -0300

    Arreglo de la clase TodaysDate con atributos modificados

--- src/main/java/TestFecha.java
+++ src/main/java/TestFecha.java
@@ -1,37 +1,35 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+
 class TodaysDate {
-    // Variable que guarda el tiempo
-    private String time;
+    // Atributos
+    String time;
+    public int day;
+    private int month;
+    protected int year;
 
-    public TodaysDate() {
-        setCurrentTime();
-    }
-
-    private void setCurrentTime() {
-        Calendar now = new GregorianCalendar();
-        int year = now.get(Calendar.YEAR);
-        int month = now.get(Calendar.MONTH) + 1;
-        int day = now.get(Calendar.DAY_OF_MONTH);
-        int hour = now.get(Calendar.HOUR_OF_DAY);
-        int minute = now.get(Calendar.MINUTE);
-        int second = now.get(Calendar.SECOND);
-
-        // se agrega el formato en el que se quiere entregar cada dato
-        this.time = String.format("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, minute, second);
-    }
-
+    // Metodo que imprime la fecha y hora actuales
     public void printDateAndTime() {
-        System.out.println("La fecha y hora actuales son: " + this.time);
+        GregorianCalendar calendar = new GregorianCalendar();
+        time = calendar.get(Calendar.HOUR_OF_DAY) + ":"
+                + calendar.get(Calendar.MINUTE) + ":"
+                + calendar.get(Calendar.SECOND);
+        day = calendar.get(Calendar.DATE);
+        month = calendar.get(Calendar.MONTH) + 1; // Los meses inician en 0, por eso se suma 1
+        year = calendar.get(Calendar.YEAR);
+
+        System.out.println("Time: " + time);
+        System.out.println("Date: " + day + " " + month + " " + year);
     }
 }
 
 public class TestFecha {
     public static void main(String[] args) {
-
+        // Crear un objeto de tipo TodaysDate
         TodaysDate hoy = new TodaysDate();
 
+        // Llamar al metodo para imprimir la fecha y hora actual
         hoy.printDateAndTime();
     }
 }
